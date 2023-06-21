e8aae7566ef20635f89c5f4cf18f92a176836a35
JonasZinne <102172816+JonasZinne@users.noreply.github.com>
2023-06-21 13:37:38 +0200

    Version 0.8

--- player.py
+++ player.py
@@ -4,9 +4,11 @@ from constants import *
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("player.png").convert_alpha()
+        self.image = pygame.image.load("Pictures/player.png").convert_alpha()
         self.rect = self.image.get_rect(center=(PLAYER_START_X, GROUND_HEIGHT))
         self.velocity = 0
+        self.speed = PLAYER_SPEED
+        self.jump_height = JUMP_HEIGHT
 
     def move(self):
         self.velocity += GRAVITY
@@ -20,12 +22,18 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             if self.rect.left > left_border.right:
-                self.rect.x -= PLAYER_SPEED
+                self.rect.x -= self.speed
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.rect.x += PLAYER_SPEED
+            self.rect.x += self.speed
         if keys[pygame.K_SPACE]:
             self.jump()
 
     def jump(self):
         if self.rect.bottom == GROUND_HEIGHT:
-            self.velocity -= JUMP_HEIGHT
+            self.velocity -= self.jump_height
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def set_jump_height(self, jump_height):
+        self.jump_height = jump_height
