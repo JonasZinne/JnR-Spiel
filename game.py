10ce166f1195a1e2698fcdd484111872fa30d358
JonasZinne <102172816+JonasZinne@users.noreply.github.com>
2023-06-21 13:34:47 +0200

    Version 0.8

--- game.py
+++ game.py
@@ -3,16 +3,13 @@ from player import Player
 from obstacle import *
 from constants import *
 
-VERSION = 0.7
+VERSION = 0.8
 
 pygame.init()
 
 # Static texts
 start_text = pygame.font.SysFont(None, 100).render("Klicke, um das Spiel zu starten", True, black)
 start_text_rect = start_text.get_rect(center=(screen_width // 2, screen_height // 2))
-
-version_text = pygame.font.SysFont(None, 25).render("Version: " + str(VERSION), True, black)
-version_text_rect = version_text.get_rect(topright=(screen_width - 10, 10))
 
 score = 0
 score_text = pygame.font.SysFont(None, 150).render("Score: " + str(score), True, black)
@@ -21,6 +18,29 @@ score_text_rect = score_text.get_rect(center=(screen_width // 2, 50))
 level = 1
 level_text = pygame.font.SysFont(None, 100).render("Level: " + str(level), True, black)
 level_text_rect = level_text.get_rect(center=(screen_width // 2, 150))
+
+version_text = pygame.font.SysFont(None, 25).render("Version: " + str(VERSION), True, black)
+version_text_rect = version_text.get_rect(bottomleft=(10, screen_height - 10))
+
+def new_game():
+    global game_running, game_started, score, level
+    game_running, game_started = True, False
+    player.rect.center = (PLAYER_START_X, GROUND_HEIGHT)
+    obstacle_x_positions = []
+    obstacle_group.empty()
+    score = 0
+    level = 1
+    player.set_speed(16)
+    player.set_jump_height(16)
+
+    for i in range(NUM_OBSTACLES):
+        if i == 0:
+            x = OBSTACLE_START_X
+        else:
+            x = obstacle_x_positions[i - 1] + random.randint(MIN_DISTANCE, MAX_DISTANCE)
+
+        obstacle_x_positions.append(x)
+        obstacle_group.add(Obstacle(x, GROUND_HEIGHT))
 
 # Game Loop
 game_running, game_started = True, False
@@ -64,7 +84,7 @@ while game_running:
     screen.blit(player.image, player.rect.move(camera_x, 0))
 
     # Ground
-    ground_image = pygame.image.load("ground.png").convert_alpha()
+    ground_image = pygame.image.load("Pictures\ground.png").convert_alpha()
     ground_image = pygame.transform.scale(ground_image, (screen_width, 100))
     screen.blit(ground_image, (0, GROUND_HEIGHT))
 
@@ -72,20 +92,21 @@ while game_running:
     if player.rect.left > obstacle_x_positions[score]:
         score += 1
         score_text = pygame.font.SysFont(None, 150).render("Score: " + str(score), True, black)
-        
+
         if score >= 30 and score < 60:
-            PLAYER_SPEED = 15
-            JUMP_HEIGHT = 15
-            level = 2    
+            player.set_speed(15)
+            player.set_jump_height(15)
+            level = 2
         elif score >= 60 and score < 90:
-            PLAYER_SPEED = 14
-            JUMP_HEIGHT = 14
+            player.set_speed(14)
+            player.set_jump_height(14)
             level = 3
         elif score >= 90 and score < 100:
-            PLAYER_SPEED = 13
-            JUMP_HEIGHT = 13
-            level = 4 
-        level_text = pygame.font.SysFont(None, 100).render("Level: " + str(level), True, black)
+            player.set_speed(13)
+            player.set_jump_height(13)
+            level = 4
+
+    level_text = pygame.font.SysFont(None, 100).render("Level: " + str(level), True, black)
 
     screen.blit(score_text, score_text_rect)
     screen.blit(level_text, level_text_rect)
@@ -97,28 +118,7 @@ while game_running:
         screen.blit(game_over_text, game_over_text_rect)
         pygame.display.update()
         time.sleep(2)
-
-        # Start new game
-        game_running, game_started = True, False
-        player.rect.center = (PLAYER_START_X, GROUND_HEIGHT)
-        obstacle_x_positions = []
-        obstacle_group.empty()
-        score = 0
-        level = 1
-        PLAYER_SPEED = 16
-        JUMP_HEIGHT = 16
-
-        if event.type == pygame.QUIT:  # Close window
-            game_running = False
-
-        for i in range(NUM_OBSTACLES):
-            if i == 0:
-                x = OBSTACLE_START_X
-            else:
-                x = obstacle_x_positions[i - 1] + random.randint(MIN_DISTANCE, MAX_DISTANCE)
-
-            obstacle_x_positions.append(x)
-            obstacle_group.add(Obstacle(x, GROUND_HEIGHT))
+        new_game()
 
     # Victory
     if score >= NUM_OBSTACLES:
@@ -127,28 +127,7 @@ while game_running:
         screen.blit(victory_text, victory_text_rect)
         pygame.display.update()
         time.sleep(5)
-
-        # Start new game
-        game_running, game_started = True, False
-        player.rect.center = (PLAYER_START_X, GROUND_HEIGHT)
-        obstacle_x_positions = []
-        obstacle_group.empty()
-        score = 0
-        level = 1
-        PLAYER_SPEED = 16
-        JUMP_HEIGHT = 16
-
-        if event.type == pygame.QUIT:  # Close window
-            game_running = False
-
-        for i in range(NUM_OBSTACLES):
-            if i == 0:
-                x = OBSTACLE_START_X
-            else:
-                x = obstacle_x_positions[i - 1] + random.randint(MIN_DISTANCE, MAX_DISTANCE)
-
-            obstacle_x_positions.append(x)
-            obstacle_group.add(Obstacle(x, GROUND_HEIGHT))
+        new_game()
 
     pygame.display.update()
     clock.tick(60)
